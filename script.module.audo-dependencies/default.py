2b592f2caddc5e1ebab962d3135c1af544a22b9d
lsellens <lsellens@gmail.com>
2015-10-02 14:02:36 -0500

    spelling error

--- script.module.audo-dependencies/default.py
+++ script.module.audo-dependencies/default.py
@@ -5,34 +5,34 @@ import xbmcaddon
 import xbmcvfs
 
 __addon__             = xbmcaddon.Addon(id='script.module.audo-dependencies')
-__dependancies__      = xbmc.translatePath(__addon__.getAddonInfo('path'))
+__dependencies__      = xbmc.translatePath(__addon__.getAddonInfo('path'))
 
 parch                         = os.uname()[4]
-pnamemapper                   = xbmc.translatePath(__dependancies__ + '/lib/Cheetah/_namemapper.so')
-pssl                          = xbmc.translatePath(__dependancies__ + '/lib/OpenSSL/SSL.so')
-prand                         = xbmc.translatePath(__dependancies__ + '/lib/OpenSSL/rand.so')
-pcrypto                       = xbmc.translatePath(__dependancies__ + '/lib/OpenSSL/crypto.so')
-plibcrypto                    = xbmc.translatePath(__dependancies__ + '/lib/OpenSSL/libcrypto.so.1.0.0')
-plibssl                       = xbmc.translatePath(__dependancies__ + '/lib/OpenSSL/libssl.so.1.0.0')
-plibcryptolk                  = xbmc.translatePath(__dependancies__ + '/lib/libcrypto.so.1.0.0')
-plibssllk                     = xbmc.translatePath(__dependancies__ + '/lib/libssl.so.1.0.0')
-pyenc                         = xbmc.translatePath(__dependancies__ + '/lib/_yenc.so')
-ppar2                         = xbmc.translatePath(__dependancies__ + '/bin/par2')
-punrar                        = xbmc.translatePath(__dependancies__ + '/bin/unrar')
+pnamemapper                   = xbmc.translatePath(__dependencies__ + '/lib/Cheetah/_namemapper.so')
+pssl                          = xbmc.translatePath(__dependencies__ + '/lib/OpenSSL/SSL.so')
+prand                         = xbmc.translatePath(__dependencies__ + '/lib/OpenSSL/rand.so')
+pcrypto                       = xbmc.translatePath(__dependencies__ + '/lib/OpenSSL/crypto.so')
+plibcrypto                    = xbmc.translatePath(__dependencies__ + '/lib/OpenSSL/libcrypto.so.1.0.0')
+plibssl                       = xbmc.translatePath(__dependencies__ + '/lib/OpenSSL/libssl.so.1.0.0')
+plibcryptolk                  = xbmc.translatePath(__dependencies__ + '/lib/libcrypto.so.1.0.0')
+plibssllk                     = xbmc.translatePath(__dependencies__ + '/lib/libssl.so.1.0.0')
+pyenc                         = xbmc.translatePath(__dependencies__ + '/lib/_yenc.so')
+ppar2                         = xbmc.translatePath(__dependencies__ + '/bin/par2')
+punrar                        = xbmc.translatePath(__dependencies__ + '/bin/unrar')
 
 xbmc.log('AUDO: ' + parch + ' architecture detected', level=xbmc.LOGDEBUG)
 
-if xbmcvfs.exists(xbmc.translatePath(__dependancies__ + '/arch.x86_64')):
-    xbmcvfs.delete(xbmc.translatePath(__dependancies__ + '/arch.x86_64'))
-if xbmcvfs.exists(xbmc.translatePath(__dependancies__ + '/arch.armv6l')):
-    xbmcvfs.delete(xbmc.translatePath(__dependancies__ + '/arch.armv6l'))
-if xbmcvfs.exists(xbmc.translatePath(__dependancies__ + '/arch.armv7l')):
-    xbmcvfs.delete(xbmc.translatePath(__dependancies__ + '/arch.armv7l'))
+if xbmcvfs.exists(xbmc.translatePath(__dependencies__ + '/arch.x86_64')):
+    xbmcvfs.delete(xbmc.translatePath(__dependencies__ + '/arch.x86_64'))
+if xbmcvfs.exists(xbmc.translatePath(__dependencies__ + '/arch.armv6l')):
+    xbmcvfs.delete(xbmc.translatePath(__dependencies__ + '/arch.armv6l'))
+if xbmcvfs.exists(xbmc.translatePath(__dependencies__ + '/arch.armv7l')):
+    xbmcvfs.delete(xbmc.translatePath(__dependencies__ + '/arch.armv7l'))
 
 try:
     if xbmcvfs.exists(pnamemapper):
         xbmcvfs.delete(pnamemapper)
-    fnamemapper = xbmc.translatePath(__dependancies__ + '/lib/multiarch/_namemapper.so.' + parch)
+    fnamemapper = xbmc.translatePath(__dependencies__ + '/lib/multiarch/_namemapper.so.' + parch)
     xbmcvfs.copy(fnamemapper, pnamemapper)
     os.chmod(pnamemapper, 0755)
     xbmc.log('AUDO: Copied _namemapper.so for ' + parch, level=xbmc.LOGDEBUG)
@@ -43,7 +43,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(pssl):
         xbmcvfs.delete(pssl)
-    fssl = xbmc.translatePath(__dependancies__ + '/lib/multiarch/SSL.so.' + parch)
+    fssl = xbmc.translatePath(__dependencies__ + '/lib/multiarch/SSL.so.' + parch)
     xbmcvfs.copy(fssl, pssl)
     os.chmod(pssl, 0755)
     xbmc.log('AUDO: Copied SSL.so for ' + parch, level=xbmc.LOGDEBUG)
@@ -54,7 +54,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(prand):
         xbmcvfs.delete(prand)
-    frand = xbmc.translatePath(__dependancies__ + '/lib/multiarch/rand.so.' + parch)
+    frand = xbmc.translatePath(__dependencies__ + '/lib/multiarch/rand.so.' + parch)
     xbmcvfs.copy(frand, prand)
     os.chmod(prand, 0755)
     xbmc.log('AUDO: Copied rand.so for ' + parch, level=xbmc.LOGDEBUG)
@@ -65,7 +65,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(pcrypto):
         xbmcvfs.delete(pcrypto)
-    fcrypto = xbmc.translatePath(__dependancies__ + '/lib/multiarch/crypto.so.' + parch)
+    fcrypto = xbmc.translatePath(__dependencies__ + '/lib/multiarch/crypto.so.' + parch)
     xbmcvfs.copy(fcrypto, pcrypto)
     os.chmod(pcrypto, 0755)
     xbmc.log('AUDO: Copied crypto.so for ' + parch, level=xbmc.LOGDEBUG)
@@ -76,7 +76,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(plibcrypto):
         xbmcvfs.delete(plibcrypto)
-    flibcrypto = xbmc.translatePath(__dependancies__ + '/lib/multiarch/libcrypto.so.1.0.0.' + parch)
+    flibcrypto = xbmc.translatePath(__dependencies__ + '/lib/multiarch/libcrypto.so.1.0.0.' + parch)
     xbmcvfs.copy(flibcrypto, plibcrypto)
     os.chmod(plibcrypto, 0755)
     os.symlink(plibcrypto, plibcryptolk)
@@ -88,7 +88,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(plibssl):
         xbmcvfs.delete(plibssl)
-    flibssl = xbmc.translatePath(__dependancies__ + '/lib/multiarch/libssl.so.1.0.0.' + parch)
+    flibssl = xbmc.translatePath(__dependencies__ + '/lib/multiarch/libssl.so.1.0.0.' + parch)
     xbmcvfs.copy(flibssl, plibssl)
     os.chmod(plibssl, 0755)
     os.symlink(plibssl, plibssllk)
@@ -100,7 +100,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(pyenc):
         xbmcvfs.delete(pyenc)
-    fyenc = xbmc.translatePath(__dependancies__ + '/lib/multiarch/_yenc.so.' + parch)
+    fyenc = xbmc.translatePath(__dependencies__ + '/lib/multiarch/_yenc.so.' + parch)
     xbmcvfs.copy(fyenc, pyenc)
     os.chmod(pyenc, 0755)
     xbmc.log('AUDO: Copied _yenc.so for ' + parch, level=xbmc.LOGDEBUG)
@@ -111,7 +111,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(ppar2):
         xbmcvfs.delete(ppar2)
-    fpar2 = xbmc.translatePath(__dependancies__ + '/lib/multiarch/par2.' + parch)
+    fpar2 = xbmc.translatePath(__dependencies__ + '/lib/multiarch/par2.' + parch)
     xbmcvfs.copy(fpar2, ppar2)
     os.chmod(ppar2, 0755)
     xbmc.log('AUDO: Copied par2 for ' + parch, level=xbmc.LOGDEBUG)
@@ -122,7 +122,7 @@ except Exception, e:
 try:
     if xbmcvfs.exists(punrar):
         xbmcvfs.delete(punrar)
-    funrar = xbmc.translatePath(__dependancies__ + '/lib/multiarch/unrar.' + parch)
+    funrar = xbmc.translatePath(__dependencies__ + '/lib/multiarch/unrar.' + parch)
     xbmcvfs.copy(funrar, punrar)
     os.chmod(punrar, 0755)
     xbmc.log('AUDO: Copied unrar for ' + parch, level=xbmc.LOGDEBUG)
@@ -130,4 +130,4 @@ except Exception, e:
     xbmc.log('AUDO: Error Copying unrar for ' + parch, level=xbmc.LOGERROR)
     xbmc.log(str(e), level=xbmc.LOGERROR)
 
-xbmcvfs.File(xbmc.translatePath(__dependancies__ + '/arch.' + parch), 'w').close()
+xbmcvfs.File(xbmc.translatePath(__dependencies__ + '/arch.' + parch), 'w').close()
